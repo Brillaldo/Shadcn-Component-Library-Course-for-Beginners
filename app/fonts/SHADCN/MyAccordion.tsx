fab23d31553569cef59d51c5fca3851545fdccbf
Brillaldo Serra <brillaldoarserra@gmail.com>
2024-09-27 11:28:31 -0600

    Accordion

---
+++ app/fonts/SHADCN/MyAccordion.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+
+function MyAccordion() {
+  return (
+   
+    <div className='w-[200px]'>
+         <Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Coded Design</AccordionTrigger>
+    <AccordionContent>
+      Is a test
+    </AccordionContent>
+  </AccordionItem>
+
+  <AccordionItem value="item-2">
+    <AccordionTrigger>Other Coded Design</AccordionTrigger>
+    <AccordionContent>
+      Learn figma 
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+    </div>
+  )
+}
+
+export default MyAccordion
