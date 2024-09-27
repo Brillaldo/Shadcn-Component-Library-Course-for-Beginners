b67f2fce04c9609009ad2d09d25b469cbbafde2a
BrillaldoSe <brillaldoarserra@gmail.com>
2024-09-27 14:02:01 -0600

    Aspect de mi commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import MyAccordion from './fonts/SHADCN/MyAccordion'
 import MyAlert from './fonts/SHADCN/MyAlert'
 import { useSearchParams } from 'next/navigation'
 import MyAlertDialog from './fonts/SHADCN/MyAlertDialog'
+import MyAspectRadio from './fonts/SHADCN/MyAspectRadio'
 
 function Page() {
 
@@ -26,6 +27,10 @@ function Page() {
       
       <MyAlertDialog trigger='CODE UWI' title='Coded Uwi' content={<MyAccordion/>}/>
       <MyAlertDialog trigger='CODE DESIGN' title='Coded Design' content={ <MyAlert/>}/>
+
+      <MyAspectRadio/>
+
+
 
     </div>
 
